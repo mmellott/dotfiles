e5e4fe077824255aa3bffd02ef4748d073ce1e0a
Matthew Mellott <matthew.mellott@gd-ms.com>
2015-10-21 17:56:29 -0700

    Create symlinks to dotfiles

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+# create symlinks to dotfiles
 
-cp -vb bashrc ~/.bashrc
-cp -vb vimrc ~/.vimrc
+# git dir this script is stored in
+# http://stackoverflow.com/a/246128
+src="${BASH_SOURCE[0]}"
+while [ -h "$src" ]; do
+  dir="$( cd -P "$( dirname "$src" )" && pwd )"
+  src="$(readlink "$src")"
+  [[ $src != /* ]] && src="$dir/$src"
+done
+dir="$( cd -P "$( dirname "$src" )" && pwd )"
+
+ln -vsf "$dir/bashrc" ~/.bashrc
+ln -vsf "$dir/vimrc" ~/.vimrc
 
